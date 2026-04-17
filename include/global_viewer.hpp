0fd8d0a11a88e9d709eeaca08cfe5b21e59704dc
Han-Yu-Meng <steven_mhy@outlook.com>
2026-04-17 23:27:42 +0800

    fix warning

--- include/global_viewer.hpp
+++ include/global_viewer.hpp
@@ -55,6 +55,7 @@ public:
 
   void update_transform(const std::string& name, const Eigen::Matrix4f& matrix, const std::string& child, const std::string& parent) {
     (void)child;
+    (void)parent;
     std::lock_guard<std::mutex> lock(mtx_);
     transforms_to_update_[name] = matrix;
   }
